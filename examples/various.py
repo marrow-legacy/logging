c77a31f38bc93c06a09418a200276910df653b89
Alice Bevan-McGregor <alice@gothcandy.com>
2011-08-30 09:42:42 -0400

    Added WebCore example.

--- examples/various.py
+++ examples/various.py
@@ -1,4 +1,21 @@
-from marrow.logging import log
+# encoding: utf-8
+
+import sys
+
+from marrow.logging import log, Log, INFO, WARNING
+
+from marrow.util.bunch import Bunch
+
+
+if '-q' in sys.argv:
+    log = Log(level=INFO)
+
+elif '-qq' in sys.argv:
+    log = Log(level=WARNING)
+
+
+log = log.name('example')
+
 
 # normal stuff
 log.debug("Debugging message.")
@@ -27,3 +44,67 @@ log.data(path="less traveled", roads=42).info('Going for a walk')
 
 # log only fields as data -- no positional arguments
 log.info(paths=42, dolphins='thankful')
+
+
+# a spiffy WebCore 2 example:
+
+from functools import wraps
+
+def logged(fn):
+    @wraps(fn)
+    def inner(self, *args, **kw):
+        log = self.log.data(controller=self.__class__.__name__, method=fn.__name__)
+        
+        log.data(args=args, kw=kw).debug()
+        
+        try:
+            result = fn(self, *args, **kw)
+        
+        except:
+            log.exceptions().error()
+            raise
+        
+        log.data(result=result).debug()
+    
+    return inner
+
+
+class BaseController(object):
+    def __init__(self, context):
+        self.ctx = context
+        self.log = log.name('request').data(
+                id = id(context.request),
+                user = context.identity.name if context.identity else None
+            )
+
+
+class RootController(BaseController):
+    @logged
+    def index(self):
+        self.log.info("Inside request.")
+        return './templates/epic.html', dict(name="Epic page.")
+    
+    @logged
+    def explode(self):
+        1/0
+
+
+RootController(Bunch(request="mock1", identity=None)).index()
+RootController(Bunch(request="mock2", identity=Bunch(name='amcgregor'))).index()
+
+
+try:
+    ctx = Bunch(request="mock3", identity=Bunch(id='amcgregor'))
+    RootController(ctx)
+
+except:
+    log.exceptions().data(controller=RootController.__name__, context=ctx).error()
+
+
+root = RootController(Bunch(request="mock4", identity=None))
+
+try:
+    root.explode()
+
+except:
+    pass
